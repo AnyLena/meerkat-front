cc9f6175c5d1d60f48958cc1a4c5b5c08bb0147d
LenaBultmann <lena.bultmann@gmx.de>
2024-02-26 13:01:25 +0100

    adding event view

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = {
+    "_id": "65dc6cfd1ff7947f05ab0f49",
+    "owner": "65d8b08c0806ca2aca3837d9",
+    "participants": [],
+    "title": "Some really awesome event",
+    "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr...",
+    "location": {
+      "description": "At my House",
+      "lat": 0,
+      "lang": 0
+    },
+    "date": {
+      "start": 1708950600000,
+      "end": 1708961400000
+    },
+    "picture": "1",
+    "messages": [],
+    "todos": [
+      {
+        "done": false,
+        "title": "Bring Food",
+        "_id": "65dc6cfd1ff7947f05ab0f4a"
+      },
+      {
+        "done": false,
+        "title": "Bring Drinks",
+        "_id": "65dc6cfd1ff7947f05ab0f4b"
+      },
+      {
+        "done": false,
+        "title": "Bring something to sit on",
+        "_id": "65dc6cfd1ff7947f05ab0f4c"
+      }
+    ],
+    "created": 1708944637537,
+    "__v": 0
+  };
--- src/views/Event.jsx
+++ src/views/Event.jsx
@@ -1,5 +1,115 @@
+import { useEffect, useState } from "react";
+import { data } from "../data.js";
+import BG1 from "../assets/background/bg1.jpg";
+import Envelope from "../assets/envelope.png";
+import "../styles/event.css";
+
 const Event = () => {
-    return ( <>Event</>  );
-}
- 
+  const SERVER = import.meta.env.VITE_DB;
+  const [loading, setLoading] = useState(false);
+  //   const [eventId, setEventId] = useState("65d8b3cd37638758b13131c4");
+  //   const [userId, setUserId] = useState("65d8ac4a8dc48aae7b4e86e0");
+  const [eventData, setEventData] = useState({});
+
+  const [start, setStart] = useState({});
+  const [end, setEnd] = useState({});
+
+  const convertDate = (timestamp) => {
+    const date = new Date(timestamp);
+    const day = String(date.getDate()).padStart(2, "0");
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "Oktober",
+      "November",
+      "December",
+    ];
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const weekday = days[date.getDay()];
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+
+    return { day, month, year, weekday, hours, minutes };
+  };
+
+  //   const getEvent = async () => {
+  //     try {
+  //       setLoading(true);
+  //       const response = await fetch(`${SERVER}/events/${eventId}`);
+  //       const data = await response.json();
+  //       setEventData(data)
+  //     } catch (error) {
+  //       console.log(error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  useEffect(() => {
+    // getEvent()
+    setEventData(data);
+    setStart(convertDate(data.date.start));
+    setEnd(convertDate(data.date.end));
+  }, []);
+
+  useEffect(() => {
+    console.log(eventData);
+  }, [eventData]);
+
+  return (
+    <>
+      {Object.keys(eventData).length > 0 ? (
+        <section className="event">
+          <div className="header">
+            <h1>{eventData.title}</h1>
+          </div>
+          <section className="info">
+            <div className="time">
+              <p>{start.day}</p>
+              <p>{start.month}</p>
+              <p>{start.weekday}</p>
+              <p>
+                {start.hours}:{start.minutes} – {end.hours}:{end.minutes}
+              </p>
+              <img src={Envelope} alt="" />
+            </div>
+
+            <div className="participants"></div>
+
+            <div className="description">
+              <p>{eventData.description}</p>
+            </div>
+
+            {eventData.todos.length > 0 ? (
+              <div className="toDoList">
+                <h2>TO-DO-List</h2>
+                {eventData.todos.map((todo, index) => (
+                  <li key={index}> {todo.title} </li>
+                ))}
+              </div>
+            ) : null}
+          </section>
+        </section>
+      ) : null}
+    </>
+  );
+};
+
 export default Event;
